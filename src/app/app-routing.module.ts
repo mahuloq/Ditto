7bd1071cc9e63b3793f217d46947c834a8fe3df0
mahuloq <95161346+mahuloq@users.noreply.github.com>
2023-11-27 20:27:34 -0600

    small change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'ditti', loadChildren: () => import('./ditti/ditti.module').then(m => m.DittiModule) }, { path: 'make-ditti', loadChildren: () => import('./make-ditti/make-ditti.module').then(m => m.MakeDittiModule) }];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'ditti',
+    loadChildren: () =>
+      import('./ditti/ditti.module').then((m) => m.DittiModule),
+  },
+  {
+    path: 'make-ditti',
+    loadChildren: () =>
+      import('./make-ditti/make-ditti.module').then((m) => m.MakeDittiModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
