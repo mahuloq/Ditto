21b467f5f0ae5f5d9d18ea9bf3548ab70b519e79
Matt Tucker <arcanemahuloq@gmail.com>
2023-11-28 22:06:53 -0600

    Created Data service, stored firebase info in service. Working on Formgroups, not importing reactive modules correctly

--- src/app/make-ditti/make-ditti.component.ts
+++ src/app/make-ditti/make-ditti.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-make-ditti',
   templateUrl: './make-ditti.component.html',
-  styleUrls: ['./make-ditti.component.css']
+  styleUrls: ['./make-ditti.component.css'],
 })
-export class MakeDittiComponent {
+export class MakeDittiComponent implements OnInit {
+  dittiForm: FormGroup;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dittiForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+    });
+  }
 }
