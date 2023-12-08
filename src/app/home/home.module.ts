cce34270bad4b5ca15e3723023c017694e1b08d2
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-12-07 18:20:45 -0600

    Add header to shared

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
-import { HeaderComponent } from './header/header.component';
+
 
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
