9c43ce40a69c0a4685055661ba48e7a7884d9a53
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-12-08 17:31:47 -0600

    Update home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
+
 import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule, BrowserModule, HttpClientModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, HttpClientModule],
 })
 export class HomeModule {}
