aba9935e8849ed7b2c9fd21050401c917736a964
Paul Theby <paultheby@gmail.com>
2023-12-07 18:46:32 -0600

    Navigate user after login/signup
    Navigates user to home screen after login and sign up

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'app/auth/auth.service';
 import { passwordMatchValidator } from 'app/auth/confirm-password.validator';
 
@@ -15,7 +16,7 @@ export class SignupComponent {
   error:string= null;
 
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
 
@@ -48,6 +49,7 @@ export class SignupComponent {
       (resData) => {
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['home']);
       },
       errorMessage => {
         console.log(errorMessage);
