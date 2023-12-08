c21b6dbfba2b796b593243284b054051ce2a2fbb
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-12-08 17:09:45 -0600

    Got Dittis to display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,21 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from 'app/shared/data-storage.service';
 import { Ditti } from 'app/shared/ditti.model';
 
+;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
- ditties: Ditti[] = []
+ dittis: Ditti[] = []
   constructor(private dataStorageService: DataStorageService) {}
-  ngOnInit(): void {
-    this.getDittis();
+  ngOnInit()  {this.getDittis();
+
+
   }
 
-  private getDittis(): void {
-    this.dataStorageService.getDittis().subscribe((data)=>{
-console.log (data)
+   getDittis(): void {
+    this.dataStorageService.getDittis().subscribe((data:Ditti[])=>{
+this.dittis = data;
+console.log(data)
     })
     console.log('Dittis:');
   }
