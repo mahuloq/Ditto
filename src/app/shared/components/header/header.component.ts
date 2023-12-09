a2dacbc604278bfb5a1a2ca40456aabf2a76d0ef
Paul Theby <paultheby@gmail.com>
2023-12-08 23:09:43 -0600

    Copied code from home to display dittis on header
    Header might need a routing module and module to be able to work. Added some sign up,login,logout buttons to header. Added a little bootstrap styling to the home page list items.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from 'app/shared/data-storage.service';
+import { Ditti } from 'app/shared/ditti.model';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  dittis: Ditti[] = []
+  constructor(private dataStorageService: DataStorageService) {}
+  ngOnInit()  {this.getDittis();
 
+
+  }
+
+   getDittis(): void {
+    this.dataStorageService.getDittis().subscribe((data:Ditti[])=>{
+this.dittis = data;
+console.log(data)
+    })
+    console.log('Dittis:');
+  }
 }
+
