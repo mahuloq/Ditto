408722d836c407579cd53397541d7b853df10569
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-12-09 14:59:11 -0600

    Added created dittis to header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from 'app/shared/data-storage.service';
+import { Ditti } from 'app/shared/ditti.model';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {dittis: Ditti[] = []
+  constructor(private dataStorageService: DataStorageService) {}
+  ngOnInit()  {this.getDittis(); }
+
+  getDittis(): void {
+    this.dataStorageService.getDittis().subscribe((data:Ditti[])=>{
+this.dittis = data;
+console.log(data)
+    })
+    console.log('Dittis:');
+  }
 
 }
