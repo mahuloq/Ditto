5e8ef9d2cfa5d8ed7c955e66b4a93d74d1ba0ece
Matt Tucker <arcanemahuloq@gmail.com>
2023-12-09 23:06:59 -0600

    Edited data service, to get make ditti and ditti working again

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from 'app/shared/data-storage.service';
+import { DittiService } from 'app/shared/ditti-service.service';
 import { Ditti } from 'app/shared/ditti.model';
 
 @Component({
@@ -9,16 +10,22 @@ import { Ditti } from 'app/shared/ditti.model';
 })
 export class HeaderComponent implements OnInit {
   dittis: Ditti[] = [];
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private dittiService: DittiService
+  ) {}
   ngOnInit() {
-    this.getDittis();
+    // this.getDittis();
+    this.dittiService.dittiListChanged.subscribe(
+      (data) => (this.dittis = data)
+    );
   }
 
-  getDittis(): void {
-    this.dataStorageService.getDittis().subscribe((data: Ditti[]) => {
-      this.dittis = data;
-      console.log(data);
-    });
-    console.log('Dittis:');
-  }
+  // getDittis(): void {
+  //   this.dataStorageService.getDittis().subscribe((data: Ditti[]) => {
+  //     this.dittis = data;
+  //     console.log(data);
+  //   });
+  //   console.log('Dittis:');
+  // }
 }
