636d5a46cc1f9b8f713427742decc0e341bcaeb1
Paul Theby <paultheby@gmail.com>
2023-12-09 22:02:11 -0600

    Signup/Login Disappear When Logged In-pushed to right of screen
    Subscribed header to user auth status in by injecting auth service and then creating that user in header ts. when authenticated it will remove the login and sign up button only leaving logout and logout will not be there when user is not authenticated

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AppRoutingModule } from 'app/app-routing.module';
+
+import { AuthService } from 'app/auth/auth.service';
 import { DataStorageService } from 'app/shared/data-storage.service';
 import { Ditti } from 'app/shared/ditti.model';
+import {  Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+isAuthenticated=false;
+private userSub:Subscription;
+
+
   dittis: Ditti[] = [];
-  constructor(private dataStorageService: DataStorageService, private router: Router) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
   ngOnInit() {
+this.userSub=this.authService.user.subscribe(user => {
+  this.isAuthenticated=!!user;
+  console.log(!user);
+  console.log(!!user);
+})
+
     this.getDittis();
   }
 
@@ -29,5 +45,7 @@ export class HeaderComponent implements OnInit {
   login() {
     this.router.navigate(['/auth/login']);
   }
-
+  ngOnDestroy(): void {
+      this.userSub.unsubscribe();
+  }
 }
