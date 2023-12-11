bcd185dc06cb8f53a2c583e3b7934278a9cef12e
Paul Theby <paultheby@gmail.com>
2023-12-10 21:54:47 -0600

    Auth Guard Working on make-ditti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,11 +14,13 @@ const routes: Routes = [
   },
   {
     path: 'ditti',
+
     loadChildren: () =>
       import('./ditti/ditti.module').then((m) => m.DittiModule),
   },
   {
     path: 'make-ditti',
+    canActivate:[AuthGuard],
     loadChildren: () =>
       import('./make-ditti/make-ditti.module').then((m) => m.MakeDittiModule),
   },
