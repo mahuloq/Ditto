ef92fd7dc6dcfb1fbb1ca1f2379d7bf37a48404a
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-12-12 13:50:55 -0600

    Added routing to header

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from 'app/shared/components/header/header.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'ditti/:name', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { AuthService } from 'app/auth/auth.service';
 import { DataStorageService } from 'app/shared/data-storage.service';
@@ -15,33 +15,31 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
   private userSub: Subscription;
-
-
+  private routeSub = new Subscription();
   dittis: Ditti[] = [];
-
-
 
   constructor(
     private router: Router,
     private authService: AuthService,
-    private dittiService: DittiService
+    private dittiService: DittiService,
+    private route: ActivatedRoute
   ) {}
   ngOnInit() {
-
     this.userSub = this.authService.user.subscribe((user) => {
       this.isAuthenticated = !!user;
       console.log(!user);
       console.log(!!user);
-     
     });
 
     this.dittiService.dittiListChanged.subscribe(
       (data) => (this.dittis = data)
     );
+    this.routeSub = this.route.paramMap.subscribe((params) => {
+      const dittiName = params.get('name');
+    });
   }
   onLogout() {
     this.authService.logout();
-
   }
 
   signUp() {
@@ -53,5 +51,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.userSub.unsubscribe();
+  }
+  navigateToDitti(dittiName: string) {
+    this.router.navigate(['/ditti', dittiName]);
   }
 }
