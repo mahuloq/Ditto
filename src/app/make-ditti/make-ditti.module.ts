3da9439577bac89ee84eae9e4ce812be05b0b61d
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-12-19 18:32:44 -0600

    Fixed issues with make ditti and home..module.ts

--- src/app/make-ditti/make-ditti.module.ts
+++ src/app/make-ditti/make-ditti.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MakeDittiRoutingModule } from './make-ditti-routing.module';
 import { MakeDittiComponent } from './make-ditti.component';
 
-
 @NgModule({
-  declarations: [
-    MakeDittiComponent
-  ],
+  declarations: [MakeDittiComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
-    MakeDittiRoutingModule
-  ]
+    MakeDittiRoutingModule,
+  ],
 })
-export class MakeDittiModule { }
+export class MakeDittiModule {}
